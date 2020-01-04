c683aecdf36cb1a33a0d432b0a2b6c7ef22f4105
Matthieu Coulon <matthieu.coulon@paygreen.fr>
2020-01-04 22:56:53 +0100

    feat: update theme depending chakraui

--- src/theme/dark.theme.js
+++ src/theme/dark.theme.js
@@ -1,19 +1,8 @@
+import { theme } from '@chakra-ui/core';
+
 export const DarkTheme = {
-    name: 'dark',
-    color: {
-        white: '#fff',
-        black: '#000',
-        lightGray: '#ececec',
-        purple: '#744c9d',
-        main: 'blue',
-        backgroundColor: '#2d3436',
-        header: '#0a3d62',
-    },
-    button: {},
-    size: {
-        inner: '1260px',
-        borderRadius: '4px',
-        borderWidth: '2px',
-        padding: '10px',
+    ...theme,
+    colors: {
+        ...theme.colors,
     },
 };
--- src/theme/default.theme.js
+++ src/theme/default.theme.js
@@ -1,19 +1,8 @@
+import { theme } from '@chakra-ui/core';
+
 export const LightTheme = {
-    name: 'light',
-    color: {
-        white: '#fff',
-        black: '#000',
-        lightGray: '#ececec',
-        purple: '#744c9d',
-        main: '#744c9d',
-        backgroundColor: '#fff',
-        header: '#60a3bc',
-    },
-    button: {},
-    size: {
-        inner: '1260px',
-        borderRadius: '4px',
-        borderWidth: '2px',
-        padding: '10px',
+    ...theme,
+    colors: {
+        ...theme.colors,
     },
 };
