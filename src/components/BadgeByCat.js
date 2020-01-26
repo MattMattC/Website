1a7e672ea5d37ebd40722664d0902c44bf982d5a
MattMattC <roadfen@gmail.com>
2020-01-26 21:44:03 +0100

    feat: create switcher Badge color category

---
+++ src/components/BadgeByCat.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Badge, useColorMode } from '@chakra-ui/core';
+
+const BadgeByCat = ({ cat }) => {
+    const { colorMode } = useColorMode();
+    let colorCat = { light: 'blue', dark: 'gray' };
+
+    switch (cat) {
+        case 'js':
+        case 'javascript':
+            colorCat = { light: 'yellow', dark: 'yellow' };
+            break;
+        case 'photo':
+            colorCat = { light: 'red', dark: 'red' };
+            break;
+        case 'tech':
+            colorCat = { light: 'red', dark: 'red' };
+            break;
+        case 'my-life':
+            colorCat = { light: 'red', dark: 'red' };
+            break;
+    }
+
+    return (
+        <Badge mr="1" variantColor={colorCat[colorMode]}>
+            {cat}
+        </Badge>
+    );
+};
+
+BadgeByCat.propTypes = {
+    /**
+     * Category string
+     */
+    cat: PropTypes.string.isRequired,
+};
+
+export default BadgeByCat;
