4f001e4fd8c353ca6549822973503f7d6c673be8
MattMattC <roadfen@gmail.com>
2020-03-08 17:39:40 +0100

    Updates

--- component---src-pages-page-2-js-d87199091b058a15a909.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{225:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(91),c=a(222),r=a(221);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-d87199091b058a15a909.js.map
